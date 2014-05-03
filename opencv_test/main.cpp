84198044534d8582ecf8cb0d50756209adcc6dc2
Watson <Watson@lyks-MacBook-Pro.local>
2014-05-03 16:36:43 +0800

    Initial Commit

---
+++ opencv_test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  opencv_test
+//
+//  Created by Watson on 5/3/14.
+//  Copyright (c) 2014 Watson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
